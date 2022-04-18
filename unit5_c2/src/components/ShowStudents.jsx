296e8300233f1cd5fa0d4ab941011ac0009fa029
Neelam <neelam0101singh@gmail.com>
2022-04-18 23:22:22 +0530

    added

---
+++ unit5_c2/src/components/ShowStudents.jsx
@@ -0,0 +1,98 @@
+import { useState ,useEffect } from "react";
+export const ShowStudents = () => {
+    const [result,setresult] = useState([])
+
+  
+    useEffect(
+  ()=>{
+      fetch("http://localhost:8080/students")
+      .then((d)=>d.json())
+      .then((data)=>{
+          data.sort(function(a,b){
+              const nameA=a.first_name;
+              const nameB=b.first_name;
+              if(nameA< nameB){
+                 return -1
+              }
+              if(nameA> nameB){
+                  return 1
+               }
+          })
+           setresult(data)
+      })
+      },[]
+      )
+
+
+    return (
+      <div>
+        <div className="controls">
+          <div>
+            Sort By:{" "}
+            <select
+              // select dropdown needs both value and onChange
+              className="sortby"
+            >
+              <option value="first_name">First Name</option>
+              <option value="gender">Gender</option>
+              <option value="age">Age</option>
+              <option value="tenth_score">10th Score</option>
+              <option value="twelth_score">12th Score</option>
+            </select>
+          </div>
+          <div>
+            Order:
+            <select
+              // select dropdown needs both value and onChange
+              className="sortorder"
+            >
+              <option value="asc">Ascending</option>
+              <option value="desc">Descending</option>
+            </select>
+          </div>
+          <button className="sort" onClick={()=> 
+          
+
+
+        {  let a = [...result].sort((a,b)=>(Number(a.age)-Number(b.age)));
+             setresult(a)
+          }
+            
+             
+          }
+          >sort</button>
+        </div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Gender</th>
+              <th>Age</th>
+              <th>10th Score</th>
+              <th>12th Score</th>
+              <th>Branch</th>
+            </tr>
+          </thead>
+          
+          <tbody className="tbody">
+            {/* populate all rows like below: */}
+            {/* {console.log(result.sort((a,b)=>(Number(a.age)-Number(b.age))))} */}
+
+            {result.map((d)=>(
+            <tr className="row">
+              <td className="first_name">{d.first_name}</td>
+              <td className="last_name">{d.last_name}</td>
+              <td className="email">{d.email}</td>
+              <td className="gender">{d.gender}</td>
+              <td className="age">{d.age}</td>
+              <td className="tenth_score">{d.tenth_score}</td>
+              <td className="twelth_score">{d.twelth_score}</td>
+              <td className="preferred_branch">{d.preferred_branch}</td>
+            </tr>))}
+          </tbody>
+        </table>
+      </div>
+    );
+  };
